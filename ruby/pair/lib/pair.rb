dc4ddfebc597fd3857c621126ad06a7ec7fd5e0d
jchaput <piinkyz05@gmail.com>
2017-02-27 13:47:18 -0500

    Updated fiddle import path

--- ruby/pair/lib/pair.rb
+++ ruby/pair/lib/pair.rb
@@ -1,3 +1,3 @@
 require 'fiddle'
-library = Fiddle::dlopen('native/libpair.dylib')
+library = Fiddle::dlopen("#{File.dirname(__FILE__)}/../native/libpair.dylib")
 Fiddle::Function.new(library['initialize'], [], Fiddle::TYPE_VOIDP).call
